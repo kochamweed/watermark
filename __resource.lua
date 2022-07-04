bd88a209ef869e3d8e2fcb5dd76f9d1529627aa9
mlodysvko <91133634+mlodysvko@users.noreply.github.com>
2022-07-04 20:18:09 +0200

    watermark

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,4 @@
-fx_version 'cerulean'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'watermark on FiveM SERVER'
 
@@ -7,6 +7,7 @@ ui_page 'html/ui.html'
 client_script 'client.lua'
 server_scripts {
 	'server.lua',
+	'@mysql-async/lib/MySQL.lua'
 }
 
 files {
